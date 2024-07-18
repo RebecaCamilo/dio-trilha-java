5dedb073b2db666148002840c57d02787ca25d3b
RebecaCamilo <rebecacamilo3@gmail.com>
2024-07-18 16:14:42 -0300

    cria conta no terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        var conta = new Conta();
+        
+        System.out.println("Por favor, digite o a agência:");
+        conta.setAgencia(sc.nextLine());
+        
+        System.out.println("Por favor, digite o nome do cliente:");
+        conta.setNomeCliente(sc.nextLine());
+
+        System.out.println("Por favor, digite o número:");
+        conta.setNumero(sc.nextInt());
+
+        System.out.println("Por favor, digite o saldo:");
+        conta.setSaldo(sc.nextBigDecimal());
+
+        System.out.println("Olá " + conta.getNomeCliente() + ", obrigado por criar uma conta " +
+                            "em nosso banco, sua agência é " + conta.getAgencia() + ", conta "
+                            + conta.getNumero() + " e seu saldo R$" + conta.getSaldo() +
+                            " já está disponível para saque.");
+
+        sc.close();
     }
+
+    
 }
