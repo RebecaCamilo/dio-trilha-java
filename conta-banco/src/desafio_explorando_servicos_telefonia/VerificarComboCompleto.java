1fa1db75cff58a943362c9813b7cb334ca333941
RebecaCamilo <rebecacamilo3@gmail.com>
2024-08-14 23:22:32 -0300

    refac: polish comments

--- conta-banco/src/desafio_explorando_servicos_telefonia/VerificarComboCompleto.java
+++ conta-banco/src/desafio_explorando_servicos_telefonia/VerificarComboCompleto.java
@@ -7,29 +7,22 @@ public class VerificarComboCompleto {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitando ao usuário a lista de serviços contratados
         String input = scanner.nextLine();
 
-        // Convertendo a entrada em uma lista de strings
         String[] servicosContratados = input.split(",");
 
-        // Verificando se o cliente contratou um combo completo
         String resultado = verificarComboCompleto(servicosContratados);
 
-        // Exibindo o resultado
         System.out.println(resultado);
 
-        // Fechando o scanner
         scanner.close();
     }
 
     public static String verificarComboCompleto(String[] servicosContratados) {
-        // Variáveis booleanas para verificar a contratação de cada serviço
         boolean movelContratado = false;
         boolean bandaLargaContratada = false;
         boolean tvContratada = false;
 
-        // TODO: Itere sobre os serviços contratados
         for (String servico : servicosContratados) {
             if(servico.equals("movel")) {
                 movelContratado = true;
@@ -42,7 +35,6 @@ public class VerificarComboCompleto {
             }
         }
 
-        // TODO: Verifique se todos os serviços foram contratados
         if (movelContratado && bandaLargaContratada && tvContratada) {
             return "Combo Completo";
         } else {
